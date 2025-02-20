996f9f0e159629c42e47339727626b474cf6402e
JUN-1999 <https://github.com/JUN-1999>
2025-02-20 16:36:10 +0800

    打包完成

--- settings.py
+++ settings.py
@@ -10,16 +10,16 @@ class Settings():
        
         
         # 子弹设置
-        self.bullet_speed_factor = 1
+        self.bullet_speed_factor = 10 # 子弹速度
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
         
         # 敌机设置
         # 左右移动速度
-        self.alien_speen_factor = 0.1
+        self.alien_speen_factor = 1 # 敌机左右移动速度
         # 下落速度
-        self.fleet_drop_speed = 20
+        self.fleet_drop_speed = 20 # 敌机下落速度
         # fleet_direction为1表示向右移，为-1表示向左移
         self.fleet_direction = 1
 
@@ -27,4 +27,4 @@ class Settings():
         # 玩家生命值
         self.ship_limit=3
         # 移动速度
-        self.ship_speed_factor = 0.5
+        self.ship_speed_factor = 3 
